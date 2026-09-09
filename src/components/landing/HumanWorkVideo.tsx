c44af5ed63898479baee8a9b8ad4894398a7b28f
aimenelh <aimenoffi@gmail.com>
2026-09-09 23:18:02 +0200

    Remplacer la vidéo hero par une équipe plus jeune et dynamique

--- src/components/landing/HumanWorkVideo.tsx
+++ src/components/landing/HumanWorkVideo.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Logomark, Wordmark } from "@/components/Brand";
 
-const VIDEO_SRC = "https://www.pexels.com/download/video/6774777/?v=6774777";
+const VIDEO_SRC = "https://www.pexels.com/download/video/8636282/?v=8636282";
 const MAX_VIDEO_SECONDS = 8;
 const END_MESSAGE = "Moins de relances. Plus de temps pour l’humain.";
 
