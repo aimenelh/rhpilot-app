71de505628920ecd4287ce18c7936ea5b75e4ae6
aimenelh <aimenoffi@gmail.com>
2026-09-13 18:27:49 +0200

    fix: trace PDFKit font metrics in Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    outputFileTracingIncludes: {
+      "/*": ["./node_modules/pdfkit/js/data/**/*"],
+    },
+  },
 };
 
 export default nextConfig;
